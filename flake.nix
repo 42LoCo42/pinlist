268130e214a330478c27d267d1259d9ea4d19a91
Leon Schumacher <leonsch@protonmail.com>
2024-08-16 21:14:30 +0200

    Fix nix build

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/080a4a27f206d07724b88da096e27ef63401a504";
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; }; in
@@ -8,7 +6,7 @@
         packages.default = pkgs.buildGoModule rec {
           name = "pinlist";
           src = ./.;
-          vendorHash = "sha256-IoLVAfAY+678IVp6yszj62NjBqPruomnHyDd8RqtFPs=";
+          vendorHash = "sha256-6LEpelMU1eGbjYHQ7LjZqZU/lUGc3tlRen8NgT5vStg=";
 
           CGO_ENABLED = "0";
           stripAllList = [ "bin" ];
